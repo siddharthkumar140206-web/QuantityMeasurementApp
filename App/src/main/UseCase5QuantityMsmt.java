b658f91ab9e6f7e66fba1f34f6574fb020afe641
siddharthkumar140206-web <siddharthkumar140206gmail.com>
2026-05-06 22:33:06 +0530

    sidharth added UC6-AdditionofTwoLengthUnis

--- App/src/main/UseCase5QuantityMsmt.java
+++ App/src/main/UseCase5QuantityMsmt.java
@@ -1,8 +1,6 @@
-package test;
+class UseCase6QuantityMsmt {
 
-public class UseCase5QuantityMsmt {
-
-    // -------- ENUM FOR UNITS --------
+    // ENUM FOR UNITS
     enum LengthUnit {
 
         FEET(12.0),
@@ -21,19 +19,18 @@ public class UseCase5QuantityMsmt {
         }
     }
 
-    // -------- LENGTH CLASS --------
+    // LENGTH CLASS
     static class Length {
 
         private final double value;
         private final LengthUnit unit;
 
-        // Constructor
         public Length(double value, LengthUnit unit) {
             this.value = value;
             this.unit = unit;
         }
 
-        // Convert to base unit (inches)
+        // Convert to inches
         private double convertToBaseUnit() {
             return value * unit.getConversionFactor();
         }
@@ -41,19 +38,27 @@ public class UseCase5QuantityMsmt {
         // Convert to another unit
         public Length convertTo(LengthUnit targetUnit) {
 
-            if (targetUnit == null) {
-                throw new IllegalArgumentException("Target unit cannot be null");
-            }
-
-            double baseValue = convertToBaseUnit();
+            double inches = convertToBaseUnit();
 
             double convertedValue =
-                    baseValue / targetUnit.getConversionFactor();
+                    inches / targetUnit.getConversionFactor();
 
             return new Length(convertedValue, targetUnit);
         }
 
-        // Equality check
+        // ADDITION METHOD
+        public Length add(Length other) {
+
+            double totalInches =
+                    this.convertToBaseUnit()
+                            + other.convertToBaseUnit();
+
+            double result =
+                    totalInches / this.unit.getConversionFactor();
+
+            return new Length(result, this.unit);
+        }
+
         @Override
         public boolean equals(Object obj) {
 
@@ -77,60 +82,36 @@ public class UseCase5QuantityMsmt {
         }
     }
 
-    // -------- STATIC CONVERSION METHOD --------
-    public static Length demonstrateLengthConversion(
-            double value,
-            LengthUnit fromUnit,
-            LengthUnit toUnit) {
+    // DEMO METHOD
+    public static void demonstrateAddition(
+            double value1,
+            LengthUnit unit1,
+            double value2,
+            LengthUnit unit2) {
 
-        Length length = new Length(value, fromUnit);
+        Length length1 = new Length(value1, unit1);
 
-        return length.convertTo(toUnit);
+        Length length2 = new Length(value2, unit2);
+
+        Length result = length1.add(length2);
+
+        System.out.println("==================================");
+        System.out.println("UC6 - Addition of Length Units");
+        System.out.println("==================================");
+
+        System.out.println("First Length : " + length1);
+        System.out.println("Second Length : " + length2);
+        System.out.println("Result : " + result);
     }
 
-    // -------- MAIN METHOD --------
+    // MAIN METHOD
     public static void main(String[] args) {
 
-        System.out.println("=======================================");
-        System.out.println("UC5 - Unit To Unit Conversion");
-        System.out.println("=======================================\n");
-
-        Length result1 =
-                demonstrateLengthConversion(
-                        1.0,
-                        LengthUnit.FEET,
-                        LengthUnit.INCHES
-                );
-
-        System.out.println("1 FOOT = " + result1);
-
-        Length result2 =
-                demonstrateLengthConversion(
-                        3.0,
-                        LengthUnit.YARDS,
-                        LengthUnit.FEET
-                );
-
-        System.out.println("3 YARDS = " + result2);
-
-        Length result3 =
-                demonstrateLengthConversion(
-                        36.0,
-                        LengthUnit.INCHES,
-                        LengthUnit.YARDS
-                );
-
-        System.out.println("36 INCHES = " + result3);
-
-        Length result4 =
-                demonstrateLengthConversion(
-                        1.0,
-                        LengthUnit.CENTIMETERS,
-                        LengthUnit.INCHES
-                );
-
-        System.out.println("1 CM = " + result4);
-
-        System.out.println("\nUC5 completed successfully...");
+        demonstrateAddition(
+                1.0,
+                LengthUnit.FEET,
+                12.0,
+                LengthUnit.INCHES
+        );
     }
 }
